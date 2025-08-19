476b2345307c31f17c97532e12234c7acae1da90
KerYagciHTL <k.yagci@students.htl-leonding.ac.at>
2025-08-19 14:50:24 +0200

    small changes

--- Kerlib/MainWindow.cs
+++ Kerlib/MainWindow.cs
@@ -8,13 +8,8 @@ public class MainWindow : Core.Window
     public MainWindow() : base("MainWindow", 800, 600)
     {
         var input = new InputField(new Point(50, 50), 200, 30);
-
-        input.BackgroundNormal = new Color(255, 255, 255);
-        input.BackgroundHover = new Color(230, 230, 230);
-        input.BackgroundFocused = new Color(200, 200, 255);
-        input.Foreground = new Color(0, 0, 0);
-
-        input.TextChanged += (s, e) =>
+        
+        input.TextChanged += (_, _) =>
         {
             Console.WriteLine("Text geändert: " + input.Text);
         };
