917ddc5866bba02480f8e4915bfc92d0587eefed
KerYagciHTL <k.yagci@students.htl-leonding.ac.at>
2025-12-09 08:40:13 +0100

    Add LinuxWindow stub implementation

---
+++ Kerlib.Window/LinuxWindow.cs
@@ -0,0 +1,59 @@
+using Kerlib.Interfaces;
+using Kerlib.Native;
+
+namespace Kerlib.Window;
+
+public sealed class LinuxWindow : INativeWindow
+{
+    private readonly string _title;
+    private int _width;
+    private int _height;
+    private Color _backgroundColor = Color.White;
+
+    public LinuxWindow(string title, int width, int height, Color? bgColor = null)
+    {
+        _title = title;
+        _width = width;
+        _height = height;
+        if (bgColor.HasValue) _backgroundColor = bgColor.Value;
+        
+        // TODO: Implement X11/Wayland window creation
+        Console.WriteLine($"[Linux] Window created: {title}");
+    }
+
+    public int GetWidth() => _width;
+    public int GetHeight() => _height;
+    public string GetTitle() => _title;
+    public void SetBackgroundColor(Color color) => _backgroundColor = color;
+
+    public event Action? Resized;
+    public event Action? Closed;
+    public event Action<Key>? KeyDown;
+    public event Action<Key>? KeyUp;
+    public event Action<IReadOnlyCollection<Key>>? KeysDown;
+    public event Action? Tick;
+    public event Action<int, int>? MouseMove;
+    public event Action<int, int>? MouseDown;
+    public event Action<int, int>? MouseUp;
+    public event Action<int, int, int>? MouseWheel;
+
+    public void Show() 
+    {
+        // TODO: Show window
+        Console.WriteLine("[Linux] Window shown");
+    }
+
+    public void Destroy() 
+    {
+        Closed?.Invoke();
+    }
+
+    public void Add(IRenderable renderable) { }
+    public void Add(RenderStack stack) { }
+    public void Remove(IRenderable renderable) { }
+
+    public void Dispose() 
+    {
+        Destroy();
+    }
+}
